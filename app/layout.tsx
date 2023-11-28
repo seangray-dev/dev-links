ce167e7099011c4e48ba675d42fc48522505083f
Sean Gray <seangray.dev@gmail.com>
2023-11-27 21:30:39 -0500

    delete asset fonts and update global font using next/google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
+import { Instrument_Sans } from 'next/font/google';
+import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Next.js and Supabase Starter Kit',
   description: 'The fastest way to build apps with Next.js and Supabase',
-}
+};
+
+const instrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+    <html lang='en' className={instrumentSans.className}>
+      <body className='bg-background text-foreground'>
+        <main className='min-h-screen flex flex-col items-center'>
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
