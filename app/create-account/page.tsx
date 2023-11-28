97bda209c9cd46421e22fddff9f3d903ad295f5c
Sean Gray <seangray.dev@gmail.com>
2023-11-28 18:04:44 -0500

    remove commented out code

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -12,26 +12,6 @@ export default function CreateAccount({
 }: {
   searchParams: { message: string };
 }) {
-  // const signIn = async (formData: FormData) => {
-  //   'use server';
-
-  //   const email = formData.get('email') as string;
-  //   const password = formData.get('password') as string;
-  //   const cookieStore = cookies();
-  //   const supabase = createClient(cookieStore);
-
-  //   const { error } = await supabase.auth.signInWithPassword({
-  //     email,
-  //     password,
-  //   });
-
-  //   if (error) {
-  //     return redirect('/login?message=Could not authenticate user');
-  //   }
-
-  //   return redirect('/');
-  // };
-
   const signUp = async (data: { email: string; password: string }) => {
     'use server';
 
@@ -51,10 +31,8 @@ export default function CreateAccount({
 
     if (error) {
       console.log('signup error', error);
-      // return redirect('/login?message=Could not authenticate user');
     }
 
-    // return redirect('/login?message=Check email to continue sign in process');
     return redirect('/login');
   };
 
@@ -62,43 +40,6 @@ export default function CreateAccount({
     <div className='flex-1 flex flex-col w-full container bg-background md:justify-center'>
       <HeaderAuth />
       <SignupForm onSubmit={signUp} />
-
-      {/* <form
-        className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'
-        action={signIn}>
-        <label className='text-md' htmlFor='email'>
-          Email
-        </label>
-        <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          name='email'
-          placeholder='you@example.com'
-          required
-        />
-        <label className='text-md' htmlFor='password'>
-          Password
-        </label>
-        <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          type='password'
-          name='password'
-          placeholder='••••••••'
-          required
-        />
-        <button className='bg-green-700 rounded-md px-4 py-2 text-foreground mb-2'>
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className='border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2'>
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>
-            {searchParams.message}
-          </p>
-        )}
-      </form> */}
     </div>
   );
 }
