40346fedda53e4b2d7f860ef3bc5c9d246581db4
Sean Gray <seangray.dev@gmail.com>
2023-11-27 23:46:17 -0500

    add link around logo

--- components/layout/HeaderAuth.tsx
+++ components/layout/HeaderAuth.tsx
@@ -1,15 +1,18 @@
 import Logo from '@/assets/images/logo-devlinks-large.svg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const HeaderAuth = () => {
   return (
-    <header className='mb-16'>
-      <Image
-        className='mt-8'
-        src={Logo}
-        width={182.5}
-        height={40}
-        alt='dev links logo'></Image>
+    <header className='mb-16 max-w-[182.5px]'>
+      <Link href={'/'}>
+        <Image
+          className='mt-8'
+          src={Logo}
+          width={182.5}
+          height={40}
+          alt='dev links logo'></Image>
+      </Link>
     </header>
   );
 };
