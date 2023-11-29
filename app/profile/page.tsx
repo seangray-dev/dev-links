2a299c7e4c72bbc6492bd35a7e525f50a2842290
Sean Gray <seangray.dev@gmail.com>
2023-11-28 21:05:18 -0500

    add starter for profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/layout/Header';
+import Head from 'next/head';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className='w-full'>
+      <Header />
+    </div>
+  );
+}
