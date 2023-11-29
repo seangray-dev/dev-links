c432571aed107fccffd999a29eeb4bd09b9715ea
Sean Gray <seangray.dev@gmail.com>
2023-11-29 18:51:54 -0500

    add context provider for Links

---
+++ app/contexts/LinksContext.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode, createContext, useContext, useState } from 'react';
+
+type LinkItem = {
+  id: string;
+  platform: string;
+  url: string;
+};
+
+type LinksContextType = {
+  linkItems: LinkItem[];
+  addLinkItem: () => void;
+  removeLinkItem: (id: string) => void;
+  updateLinkItem: (id: string, platform: string, url: string) => void;
+};
+
+const LinksContext = createContext<LinksContextType | undefined>(undefined);
+
+export const useLinks = () => {
+  const context = useContext(LinksContext);
+  if (!context) {
+    throw new Error('useLinks must be used within a LinksProvider');
+  }
+  return context;
+};
+
+type LinksProviderProps = {
+  children: ReactNode;
+};
+
+export const LinksProvider: React.FC<LinksProviderProps> = ({ children }) => {
+  const [linkItems, setLinkItems] = useState<LinkItem[]>([]);
+
+  const addLinkItem = () => {
+    const newLink = { id: Date.now().toString(), platform: '', url: '' };
+    setLinkItems([...linkItems, newLink]);
+  };
+
+  const removeLinkItem = (id: string) => {
+    setLinkItems(linkItems.filter((item) => item.id !== id));
+  };
+
+  const updateLinkItem = (id: string, platform: string, url: string) => {
+    setLinkItems(
+      linkItems.map((item) =>
+        item.id === id ? { ...item, platform, url } : item
+      )
+    );
+  };
+
+  return (
+    <LinksContext.Provider
+      value={{ linkItems, addLinkItem, removeLinkItem, updateLinkItem }}>
+      {children}
+    </LinksContext.Provider>
+  );
+};
